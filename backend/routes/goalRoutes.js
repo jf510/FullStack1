eea3f61bb2f993fb2d0d415f2be5f8cf70934df0
Jfontaine <jfontaine19@gmail.com>
2022-07-11 15:56:07 -0400

    cleaned up routes file. Moved functionality to controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,4 +1,18 @@
 const express = require("express");
-const route = express.Router();
+const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+router.get("/", getGoals);
+
+router.post("/", setGoal);
+
+router.put("/:id", updateGoal);
+
+router.delete("/:id", deleteGoal);
 
 module.exports = router;
